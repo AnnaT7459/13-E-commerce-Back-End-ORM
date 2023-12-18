24168600507f8d62c1363fcd4ea177c7685a9a33
AnnaT7459 <141446349+AnnaT7459@users.noreply.github.com>
2023-12-17 20:55:41 -0600

    renamed const categories to catData to be more distinct, added try/catch exception and find by primary key to find 1 category by its id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -7,19 +7,27 @@ router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
   try {
-    const categories = await Category.findAll({
+    const catData = await Category.findAll({
       include: [{model: Product}]
     });
 
-    res.status(200).json(categories);
+    res.status(200).json(catData);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const catData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+    res.status(200).json(catData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
